ebae5c107b46a111d7f429e401c817c51452f974
OnurTalayhan <onur.talayhan@gmail.com>
2023-03-01 09:20:22 +0000

    added jobitem.html

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 app = Flask(__name__)
+JOBS = [
+  {
+    'id':1,
+    'title': 'Data Analyst',
+    'location': 'Ankara, Turkey',
+    'salary': 'TRL 10,000'
+  },
+   
+   {
+    'id':2,
+    'title': 'Data Scentist',
+    'location': 'Stockholm, Sweden',
+    'salary': 'SEK 50,000'
+  },
+  {
+    'id':3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+   
+  },
+  {
+    'id':4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': 'USD 120,000'
+  },
+]
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                         jobs=JOBS,
+                        company_name='Python Jobs')
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
